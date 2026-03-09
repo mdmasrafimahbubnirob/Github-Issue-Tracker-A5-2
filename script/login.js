67b42d018441a1b1edc1a093e8efdf6110055625
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-03-09 19:56:25 +0600

    login done

---
+++ script/login.js
@@ -0,0 +1,24 @@
+console.log("ok");
+
+document.getElementById("Sign-In-btn").addEventListener("click", function () {
+    // console.log("Sign-In-btn");
+
+    const UsernameInput = document.getElementById("Enter-Username");
+    const Username = UsernameInput.value;
+
+    // console.log(Username);
+
+    const passwordInput = document.getElementById("password");
+    const password = passwordInput.value;
+    // console.log(password);
+
+    if(Username == "admin" && password == "admin123"){
+        alert("login Success");
+
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("login Failed");
+        return;
+    }
+})
